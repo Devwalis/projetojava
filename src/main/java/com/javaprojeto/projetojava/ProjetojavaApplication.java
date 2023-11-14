a08f3b9440bedc1514840886a3b29dc10af192c3
Walisson oliveira <walissonoliveira1@outlook.com>
2023-11-14 02:40:13 -0300

    Inicio do Projeto

---
+++ src/main/java/com/javaprojeto/projetojava/ProjetojavaApplication.java
@@ -0,0 +1,13 @@
+package com.javaprojeto.projetojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaprojeto/projetojava/ProjetojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaprojeto.projetojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
